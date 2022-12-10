6e65e86888e7dbcff67a394d922be7da8fb8ebfd
JC-Coder <josephchimezie2003@gmail.com>
2022-12-10 20:31:56 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -22,11 +22,12 @@ async function bootstrap() {
   (async () => {
     const options: DataSourceOptions & SeederOptions = {
       type: 'postgres',
-      host: configService.get('DB_HOST'),
-      port: configService.get('DB_PORT'),
-      username: configService.get('DB_USERNAME'),
-      password: configService.get('DB_PASSWORD'),
-      database: configService.get('DB_NAME'),
+      // host: configService.get('DB_HOST'),
+      // port: configService.get('DB_PORT'),
+      // username: configService.get('DB_USERNAME'),
+      // password: configService.get('DB_PASSWORD'),
+      // database: configService.get('DB_NAME'),
+      url: process.env.DATABASE_URL,
       entities: [UserEntity],
 
       seeds: ['src/database/seeds/**/*{.ts,.js}'],
